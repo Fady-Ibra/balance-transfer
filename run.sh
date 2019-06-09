5fa35e46efb7d8e1962d061f03e649a45635cf8c
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-06-09 12:57:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,16 @@ mkdir fabric
 cd fabric
 rm -r *
 
+curl -sSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+source ~/.profile 
+nvm install v8.16.0
+
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash
+echo "" >> ~/.profile
+echo "export PATH=\"$PWD/fabric-samples/bin:\$PATH\"" >> ~/.profile
+source ~/.profile
+
+cd fabric-samples/balance-transfer
+./runApp.sh &
+sleep 60
+./testAPIs.sh 
