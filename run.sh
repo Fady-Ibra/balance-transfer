0bd46619efe9ecc36eddea73cffb098bf2fe9b63
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-06-09 14:39:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ mkdir fabric 2> /dev/null
 cd fabric
 rm -r * 2> /dev/null
 
-wget -O - https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+curl -sSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.profile 
 nvm install v8.16.0
 
-wget -O - https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash
 echo "" >> ~/.profile
 echo "export PATH=\"$PWD/fabric-samples/bin:\$PATH\"" >> ~/.profile
 source ~/.profile
@@ -17,3 +17,22 @@ cd fabric-samples/balance-transfer
 ./runApp.sh &
 sleep 60
 ./testAPIs.sh 
+
+
+ORG1_TOKEN=$(curl -s -X POST \
+  http://localhost:4000/users \
+  -H "content-type: application/x-www-form-urlencoded" \
+  -d 'username=Jim&orgName=Org1')
+
+ORG1_TOKEN=$(echo $ORG1_TOKEN | jq ".token" | sed "s/\"//g")
+
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"chaincodeName":"mycc",
+	"chaincodeVersion":"v0",
+	"chaincodeType": "golang",
+	"args":["a","100000","b","200000"]
+}'
