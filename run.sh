25473b066cc6216c2344200f62523b5f8a1f7332
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-06-09 15:27:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cd fabric
 rm -r * 2> /dev/null
 
 curl -sSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-source ~/.profile 
+source ~/.bashrc
 nvm install v8.16.0
 
 curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash
