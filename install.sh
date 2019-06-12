6c3eedea7d6a4950c0a10e99ddb6e07ed914289d
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-06-12 12:12:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ sudo apt install -y libssl-dev		&& echo install libssl-dev
 sudo apt install -y docker.io		&& echo install docker
 sudo apt install -y docker-compose	&& echo install docker-compose
 sudo gpasswd -a $USER docker		&& echo add $USER to docker group 
+curl -sSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 sudo reboot
